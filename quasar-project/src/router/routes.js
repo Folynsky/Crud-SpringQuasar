25340537519d13e38911cd56f2c0e11897d83d54
Folynsky <kevin.foly06@gmail.com>
2024-07-26 19:26:25 -0600

    Se agregan las rutas de las vistas provinientes de un proyecto de refernecia, rutas sugeteas a cambios

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -1,9 +1,48 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+    path: '/main',
+  component: () => import('layouts/MainLayout.vue'),
+  meta: { requiresAuth: true },
+  children: [
+    { path: '', component: () => import('pages/IndexPage.vue') },
+
+      //paths apartado usuarios
+      { path: '/users', component: () => import('pages/admin/users/IndexUsers.vue') },
+      { path: '/createUser', component: () => import('pages/admin/users/createUser.vue') },
+      {
+        path: '/editUser/:idusuario',
+        name: 'editUser',
+        component: () => import('pages/admin/users/editUser.vue')
+      },
+
+      //paths apartado categorias
+      { path: '/categorys', component: () => import('pages/admin/categorys/IndexCategorys.vue') },
+      { path: '/createCategory', component: () => import('pages/admin/categorys/createCategory.vue') },
+      {
+        path: '/updateCategory/:idcategoria',
+        name: 'updateCategory',
+        component: () => import('pages/admin/categorys/editCategory.vue')
+      },
+
+
+
+      //paths apartado productos
+      { path: '/products', component: () => import('pages/admin/products/IndexProducts.vue') },
+      { path: '/createProduct', component: () => import('pages/admin/products/createProduct.vue') },
+      {
+        path: '/updateProduct/:idproducto',
+        name: 'updateProduct',
+        component: () => import('pages/admin/products/editProduct.vue')
+      },
+
+      //paths apartado paqueterias
+      { path: '/shippingCompanies', component: () => import('pages/admin/shippingCompanies/IndexCompanies.vue') },
+      { path: '/createCompany', component: () => import('pages/admin/shippingCompanies/createCompany.vue') },
+      {
+        path: '/updateCompany/:idpaqueteria',
+        name: 'updateCompany',
+        component: () => import('pages/admin/shippingCompanies/updateShippingCompany.vue')
+      }
     ]
   },
 
